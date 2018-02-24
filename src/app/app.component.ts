8837df0854959ee21af5136862ef04331d76cdc7
tahina andriantomanga <tahina andriantomanga>
2018-02-24 12:30:44 +0300

    dashboard ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  private title : string = 'Ndao hifidy';
+  private drapeau : string = 'assets/images/drapeau2.png';
+
+  getColor(){
+    var num=Math.floor((Math.random() * 10) + 1);
+    switch(num) {
+      case 1:
+        return "blue";
+      case 2:
+        return "yellow darken-3";
+      case 3:
+        return "grey";
+      case 4:
+        return "green";
+      case 5:
+        return "cyan"; 
+      case 6:
+        return "indigo";  
+      case 7:
+        return "purple";  
+      case 8:
+        return "brown"; 
+      case 9:
+        return "orange"; 
+      case 10:
+        return "red darken-4";   
+    }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule,ReactiveFormsModule }   from '@angular/forms';
+import { MaterializeModule } from 'angular2-materialize';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
+const appRoutes: Routes = [
+  {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+  { path: 'dashboard', component: DashboardComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MaterializeModule
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+
+}
+
+
